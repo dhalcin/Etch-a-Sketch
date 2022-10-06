5a4d42b67cf678ea65bb2c00a1e590cbf032ef06
jhon <hankgit-h@hotmail.com>
2022-10-05 22:09:10 -0500

    mouseover/out

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 
 const square = 16*16;
 
-const container = document.querySelector('.container');
-
-function div() {
+for (let i = 0; i < square; i++) {
+    const container = document.querySelector('.container');
     const addDiv = document.createElement('div');
     addDiv.classList.add('content-div');
     container.appendChild(addDiv);
-}
 
-for (let i = 0; i < square; i++) {
-    div();
-    console.log('hola');
+    const eventDiv = document.querySelectorAll('.content-div');
+    eventDiv.forEach(eventDiv => {
+        eventDiv.addEventListener('mouseover', ()=> {
+            eventDiv.style.background = 'red';
+        });
+        eventDiv.addEventListener('mouseout', ()=>{
+            eventDiv.style.background = 'rebeccapurple';
+        });
+    });
 }
  
