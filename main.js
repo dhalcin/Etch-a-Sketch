314ef5cf3e65fee6cd634b6badbf2699951358d0
jhon-pcw <hankgit-h@hotmail.com>
2022-10-15 12:18:48 -0500

    creating dynamic div-square

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 
-const square = 16*16;
+const container = document.querySelector('.container');
+const btn = document.getElementById('btn');
 
-for (let i = 0; i < square; i++) {
-    const container = document.querySelector('.container');
-    const addDiv = document.createElement('div');
-    addDiv.classList.add('content-div');
-    container.appendChild(addDiv);
+btn.addEventListener('click', ()=> {
+    userSelection();
+});
 
-    const eventDiv = document.querySelectorAll('.content-div');
-    eventDiv.forEach(eventDiv => {
-        eventDiv.addEventListener('mouseover', ()=> {
-            eventDiv.style.background = 'red';
-        });
-        eventDiv.addEventListener('mouseout', ()=>{
-            eventDiv.style.background = 'rebeccapurple';
-        });
+function userSelection() {
+    let user = prompt("Ingresa un numero mayor de 0 y menor que 100");
+    let converValue = Number(user);
+    if (converValue === null || converValue === "") {
+        console.log("!Error ingresa un numero valido");
+    }else if (converValue === 0 || converValue === 100) {
+        console.log("!Error ingresa un numero valido");
+    }else {
+        removeDiv();
+        createDiv(converValue);
+    }
+}
+
+
+function removeDiv() {
+    const selectDiv = document.querySelectorAll('.content-div');
+    selectDiv.forEach(selectDiv => {
+        selectDiv.remove();
     });
 }
- 
+
+function createDiv(converValue) {
+    let createSquares = Math.pow(converValue, 2);
+    for (let i = 0; i < createSquares; i++) {
+        const addDiv = document.createElement('div');
+        addDiv.classList.add('content-div');
+        container.appendChild(addDiv);
+        container.style.gridTemplateColumns = `repeat(${converValue}, 1fr)`;
+
+        const eventDiv = document.querySelectorAll('.content-div');
+        eventDiv.forEach(eventDiv => {
+            eventDiv.addEventListener('mouseover', ()=> {
+                eventDiv.style.background = 'red';
+            });
+        });
+    }
+}
+
